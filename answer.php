c9a5dfc8af477a389c2667957c57a33a3b046ac5
Domenic <domenic.dangelo@stu.ocsb.ca>
2025-03-17 21:08:05 +0000

    Calculate area

--- answer.php
+++ answer.php
@@ -1,15 +1,20 @@
 <!DOCTYPE html>
-<html>
+<!-- ICS2O-Unit2-01-PHP -->
+<html lang="en-ca">
 
 <head>
   <meta charset="utf-8" />
-  <meta name="description" content="The Area & Perimeter Program, in PHP" />
+  <meta name="description" content="The 'Hello, World!' program, PHP" />
   <meta name="keywords" content="mths, icd2o" />
   <meta name="author" content="domenic dangelo" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  <link
+    rel="stylesheet"
+    href="https://code.getmdl.io/1.3.0/material.deep_purple-amber.min.css" />
   <link rel="stylesheet" href="./css/style.css" />
-  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.purple-deep_orange.min.css" />
   <link
     rel="apple-touch-icon"
     sizes="180x180"
@@ -25,7 +30,7 @@
     sizes="16x16"
     href="./favicon-16x16.png" />
   <link rel="manifest" href="./site.webmanifest" />
-  <title>The Area & Perimeter Program,in PHP</title>
+  <title>The "Hello, World!" program, PHP</title>
 </head>
 
 <body>
@@ -33,29 +38,21 @@
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">The Area & Perimeter Program, in PHP</span>
+        <span class="mdl-layout-title">The "Hello, World!" program, PHP</span>
       </div>
     </header>
     <main class="mdl-layout__content">
       <div class="right-image">
         <img src="./images/area_and_perimeter.png" alt="area and perimeter" />
       </div>
-      <div class="page-content">The dimensions of the rectangle are: 5cm and 3cm.</div>
-      <div class="page-content-php">
-        <?php echo '<p>The area is: ? </p>'; ?>
-        <?php echo '<p>The perimeter is: ? </p>'; ?>
+      <div class="page-content">
+        <?php echo '<p>The area is: ' (5 * 3) . 'cm² </p>'; ?>
+        <?php echo '<p>The perimeter is: ' (2 * (5 + 3)) . 'cm </p>'; ?>
       </div>
-      <!-- Simple Textfield for integers-->
-      <form action="answer.php" method="GET">
-        <br />
-        <!-- Accent-colored raised button with ripple -->
-        <button
-          class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
-          type="submit">
-          Calculate
-        </button>
-      </form>
       <br />
+      <div class="page-content-answer">
+        <a href="./index.php">Return</a>
+      </div>
     </main>
   </div>
 </body>
